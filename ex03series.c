7daefb02c08961c398c3aa405c2d548d8fe23ff0
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-06-07 13:46:17 -0500

    ex03

---
+++ ex03series.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h> 
+#include <math.h> 
+#include <string.h> 
+
+int main(void) {
+  int f=4;
+  float sum;
+
+  sum= (((float)f/1)-((float)f/3)+((float)f/5)-((float)f/7)+((float)f/9)-((float)f/11)+(float)f/13)-((float)f/15)+((float)f/17)-((float)f/19);
+  printf("The result of this sum is %f\n",sum);
+
+  return 0;
+
+}
