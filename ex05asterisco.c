06a7ec1ceac42dd2f540ff1c56339505213670be
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-06-08 09:06:20 -0500

    ex05 v2

--- ex05asterisco.c
+++ ex05asterisco.c
@@ -1,3 +1,14 @@
+/********************************************************
+ * Ex05 -- A program used to print a square of asterisks. *
+ * *
+ * Author: Rafael Marí. *
+ * *
+ * Purpose: To create a square made of asterisks using the properties of printf and \n. *
+ * *
+ * Usage: *
+ * Run the program and the square appears. *
+ ********************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h> 
